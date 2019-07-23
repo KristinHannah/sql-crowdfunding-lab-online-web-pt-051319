0ff0949136d73dae337f80634f6113835dbd2ec2
Kristin Kraemer <kristinhannahk@gmail.com>
2019-07-23 01:28:49 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -7,7 +7,7 @@
 # Make sure each ruby method returns a string containing a valid SQL statement.
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
-"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
+"SELECT projects.title, SUM(pledges.amount)
 FROM pledges
 INNER JOIN projects 
 ON pledges.project_id = projects.id
@@ -16,7 +16,7 @@ ORDER BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
-"SELECT users.name, users.age, SUM(pledges.amount)
+"SELECT users.name, users.age, SUM(pledges.amount) 
 FROM pledges
 INNER JOIN users
 ON pledges.user_id = users.id 
@@ -35,7 +35,7 @@ end
 #LIMIT
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"SELECT projects.title, SUM(pledges.amount)
+"SELECT projects.title, SUM(pledges.amount) - projects.funding_goal
 FROM pledges
 INNER JOIN projects 
 ON pledges.project_id = projects.id
